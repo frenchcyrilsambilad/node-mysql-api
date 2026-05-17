b26c3da14a37fec1d002e6b2b2ae3997579998df
Cyril <frenchcyrilsambilad@gmail.com>
2026-05-17 23:55:31 +0800

    Fix Swagger path on Vercel

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -2,9 +2,10 @@ import express from 'express';
 const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import path from 'path';
 
 // Load the swagger.yaml file from the project root
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load(path.join(__dirname, '..', 'swagger.yaml'));
 
 // Mount Swagger UI on the root path of this router
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
